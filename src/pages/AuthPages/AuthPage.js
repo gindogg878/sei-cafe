c53a7a53d05fc85df10d6610c5b8f0cca3caa72e
Gino Torres Jr <gindogg878@icloud.com>
2023-05-19 10:10:01 -0500

    finished project

--- src/pages/AuthPages/AuthPage.js
+++ src/pages/AuthPages/AuthPage.js
@@ -1,12 +1,25 @@
-import SignUpForm from "../../component/SignUpForm/SignUpForm";
+import { useState } from "react";
+import styles from "./AuthPage.module.css";
 import LoginForm from "../../component/LogInForm/LoginForm";
+import SignUpForm from "../../component/SignUpForm/SignUpForm";
+import Logo from "../../component/Logo/Logo";
 
 export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
-    <>
-      <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
-      <LoginForm setUser={setUser} />
-    </>
+    <main className={styles.AuthPage}>
+      <div>
+        <Logo />
+        <h3 onClick={() => setShowLogin(!showLogin)}>
+          {showLogin ? "SIGN UP" : "LOG IN"}
+        </h3>
+      </div>
+      {showLogin ? (
+        <LoginForm setUser={setUser} />
+      ) : (
+        <SignUpForm setUser={setUser} />
+      )}
+    </main>
   );
 }
--- src/pages/NewOrderpages/NewOrderPage.js
+++ src/pages/NewOrderpages/NewOrderPage.js
@@ -1,7 +1,82 @@
-export default function NewOrderPage() {
+import { useState, useEffect, useRef } from "react";
+import * as itemsAPI from "../../utilities/items-api";
+import * as ordersAPI from "../../utilities/orders-api";
+import styles from "./NewOrderPage.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../../component/Logo/Logo";
+import MenuList from "../../component/MenuList/MenuList";
+import CategoryList from "../../component/CategoryList/CategoryList";
+import OrderDetail from "../../component/OrderDetail/OrderDetail";
+import UserLogOut from "../../component/UserLogOut/UserLogOut";
+
+export default function NewOrderPage({ user, setUser }) {
+  const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState("");
+  const [cart, setCart] = useState(null);
+  const categoriesRef = useRef([]);
+  const navigate = useNavigate();
+
+  useEffect(function () {
+    async function getItems() {
+      const items = await itemsAPI.getAll();
+      //this reduce is looping thru our items and collecting all the categories that exist. It will not duplicate the categories.
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat];
+      }, []);
+      setMenuItems(items);
+      setActiveCat(categoriesRef.current[0]);
+    }
+    getItems();
+    async function getCart() {
+      const cart = await ordersAPI.getCart();
+      setCart(cart);
+    }
+    getCart();
+  }, []);
+  // Providing an empty 'dependency array'
+  // results in the effect running after
+  // the FIRST render only
+
+  /*-- Event Handlers --*/
+  async function handleAddToOrder(itemId) {
+    const updatedCart = await ordersAPI.addItemToCart(itemId);
+    setCart(updatedCart);
+  }
+
+  async function handleChangeQty(itemId, newQty) {
+    const updatedCart = await ordersAPI.setItemQtyInCart(itemId, newQty);
+    setCart(updatedCart);
+  }
+
+  async function handleCheckout() {
+    await ordersAPI.checkout();
+    navigate("/orders");
+  }
+
   return (
-    <div>
-      <h1>NewOrderPage</h1>
-    </div>
+    <main className={styles.NewOrderPage}>
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">
+          PREVIOUS ORDERS
+        </Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter((item) => item.category.name === activeCat)}
+        handleAddToOrder={handleAddToOrder}
+      />
+      <OrderDetail
+        order={cart}
+        handleChangeQty={handleChangeQty}
+        handleCheckout={handleCheckout}
+      />
+    </main>
   );
 }
